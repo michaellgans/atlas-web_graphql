47625b718869f921c09f9dfc8b8ffdd1a0b8c6fb
michaellgans <michaellgans@Victoria>
2024-07-07 15:40:37 -0500

    M - 3:40 PM - Creates schema with GraphQL

--- schema.js
+++ schema.js
@@ -1,2 +1,15 @@
-// Task 0 - 
+// Task 0 - object which contains the schema property
 
+const { GraphQLSchema, GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+
+// New Task Type
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: {
+        // ID cannot be Null
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString }
+    }
+});
