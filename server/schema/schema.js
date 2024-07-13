3f2f4eab4ffc7c664fdd64cd3971602a20690483
michaellgans <michaellgans@Victoria>
2024-07-13 15:39:30 -0500

    M - 3:39 PM - adds ProjecType to schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,9 @@
-// Task 2 - Add data array
+// Task 3 - GraphQLID and Project Type
 
-const { GraphQLSchema, GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
+const { GraphQLSchema, GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLID } = require('graphql');
 const _ = require('lodash');
 
-// New Task Type
+// Task Type
 const TaskType = new GraphQLObjectType({
     // Name is what kind of data is stored.
     // Example: Fruit
@@ -12,26 +12,35 @@ const TaskType = new GraphQLObjectType({
     // Example: color, taste, price
     fields: {
         // ID cannot be Null
-        id: { type: new GraphQLNonNull(GraphQLString) },
+        id: { type: new GraphQLNonNull(GraphQLID) },
         title: { type: GraphQLString },
         weight: { type: GraphQLInt },
         description: { type: GraphQLString }
     }
 });
 
-// Task Aray
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString }
+    }
+});
+
+// Task Aray Data Sample
 // Example: actual fruit inventory
 const taskAray = [
-    {
-        id: '1', 
-        title: 'Create your first webpage', 
-        weight: 1, 
-        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
-    {
-        id: '2', 
-        title: 'Structure your webpage', 
-        weight: 1, 
-        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'}
+    {id: 1, title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+    {id: 2, title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'}
+];
+
+// Project Aray Data Sample
+const projectAray = [
+    {id: 1,title: 'Advanced HTML', weight: 1, description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'},
+    {id: 2, title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'}
 ];
 
 // Root Query - entry point to API
@@ -50,15 +59,31 @@ const RootQueryType = new GraphQLObjectType({
         task: {
             type: TaskType,
             args: {
-                id: { type: GraphQLString }
+                id: { type: GraphQLID }
             },
             resolve(parent, args) {
                 return _.find(taskAray, { id: args.id });
             }
+        },
+        // Return more than one project
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projectAray;
+            }
+        },
+        // Return one project
+        project: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(projectAray, { id: args.id });
+            }
         }
     }
 });
-
 
 const schema = new GraphQLSchema({
     query: RootQueryType
